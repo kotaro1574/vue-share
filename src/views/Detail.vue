0947ae1ed3bc4fa5e4c05b8dade58116bbf27189
kotaro1574 <shake1574@gmail.com>
2021-01-29 10:36:56 +0900

    fix code

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -14,7 +14,7 @@
         </div>
         <div class="message" v-for="(comment, index) in data" :key="index">
           <div class="flex">
-            <p class="name">{{comment.commtent_user.name}}</p>
+            <p class="name">{{comment.comment_user.name}}</p>
           </div>
           <div>
             <p class="text">{{comment.comment.content}}</p>
